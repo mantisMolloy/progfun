d9ca713e5a9992ab457385542b0817f851f0db6a
Tom Molloy <tom.molloy@tekcomms.com>
2015-10-27 13:10:00 +0000

    finished huffman coding

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -213,7 +213,10 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = table match {
+    case Nil => throw new IllegalArgumentException("No such character")
+    case (x, bs) :: xs => if (x == char) bs else codeBits(xs)(char)
+  }
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -223,14 +226,22 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = tree match {
+    case Fork(l, r, _, _) => mergeCodeTables(convert(l), convert(r))
+    case Leaf(c, _) => List((c, Nil))
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = {
+    def step(x: Int, p: (Char, List[Bit])): (Char, List[Bit]) = p match {
+      case (c, bs) => (c, x :: bs)
+    }
+    a.map(step(0, _)) ::: b.map(step(1, _))
+  }
 
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -238,5 +249,5 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = text.flatMap(codeBits(convert(tree))(_))
 }
